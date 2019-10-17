a36aea57b8af9f4ed591251c477641c1b8ce2be6
Shimon42 <pixono26@gmail.com>
2019-10-17 22:06:54 +0200

    strtrim last abort

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,44 @@
 /*   By: siferrar <siferrar@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/09 21:13:42 by shimon       #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/17 17:18:09 by siferrar    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/17 22:06:05 by siferrar    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	iter;
+
+	iter = 0;
+	while (s[iter] != '\0')
+		iter++;
+	if (c == '\0')
+		return ((char*)&s[iter]);
+	while (iter >= 0)
+		if (s[iter--] == c)
+			return ((char*)&s[iter + 1]);
+	return (NULL);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return ((char*)&s[i]);
+		i++;
+	}
+	if ((s[i] == c && c == '\0'))
+		return ((char*)&s[i]);
+	return ((char *)NULL);
+}
+
 
 static size_t	ft_trmlen(char const *s, char const *set, int dir)
 {
@@ -21,11 +53,11 @@ static size_t	ft_trmlen(char const *s, char const *set, int dir)
 	strlen = ft_strlen(s);
 	len = 0;
 	if (dir > 0)
-		while (ft_strchr(set, s[len]) != NULL)
+		while (ft_strchr(set, s[len]) != NULL && len < strlen)
 			len++;
 	else
 	{
-		while (ft_strrchr(set, s[strlen - len + 1]) != NULL)
+		while (ft_strrchr(set, s[strlen - len - 1]) != NULL && len < strlen)
 			len++;
 	}
 	return (len);
@@ -41,12 +73,10 @@ char			*ft_strtrim(char const *s1, char const *set)
 
 	i = 0;
 	l = ft_strlen(s1);
-	set = " \n\t";
 	trmd = "";
 	trmstrt = ft_trmlen(s1, set, 1);
 	trmend = ft_trmlen(s1, set, -1);
-	printf("%lu - %lu\n", trmstrt, trmend);
-	if (l != trmstrt && l != trmend
+	if ((trmstrt < l && trmend < l)
 	&& (trmd = malloc((l - trmstrt - trmend + 1) * sizeof(char))) != NULL)
 	{
 		i = 0;
@@ -60,73 +90,3 @@ char			*ft_strtrim(char const *s1, char const *set)
 	return (trmd);
 }
 
-void	ft_print_result(char const *s)
-{
-	int		len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	write(1, s, len);
-}
-
-int		main(int argc, const char *argv[])
-{
-	char	*strtrim;
-	int		arg;
-
-	alarm(5);
-	if (argc == 1)
-		return (0);
-	else if ((arg = atoi(argv[1])) == 1)
-	{
-		char s1[] = "lorem \n ipsum \t dolor \n sit \t amet";
-		if (!(strtrim = ft_strtrim(s1, " ")))
-			ft_print_result("NULL");
-		else
-			ft_print_result(strtrim);
-		if (strtrim == s1)
-			ft_print_result("\nA new string was not returned");
-	}
-	else if (arg == 2)
-	{
-		char s1[] = "lorem ipsum dolor sit amet";
-		if (!(strtrim = ft_strtrim(s1, "te")))
-			ft_print_result("NULL");
-		else
-			ft_print_result(strtrim);
-		if (strtrim == s1)
-			ft_print_result("\nA new string was not returned");
-	}
-	else if (arg == 3)
-	{
-		char s1[] = " lorem ipsum dolor sit amet";
-		if (!(strtrim = ft_strtrim(s1, "l ")))
-			ft_print_result("NULL");
-		else
-			ft_print_result(strtrim);
-		if (strtrim == s1)
-			ft_print_result("\nA new string was not returned");
-	}
-	else if (arg == 4)
-	{
-		char s1[] = "lorem ipsum dolor sit amet";
-		if (!(strtrim = ft_strtrim(s1, "tel")))
-			ft_print_result("NULL");
-		else
-			ft_print_result(strtrim);
-		if (strtrim == s1)
-			ft_print_result("\nA new string was not returned");
-	}
-	else if (arg == 5)
-	{
-		char s1[] = "          ";
-		if (!(strtrim = ft_strtrim(s1, " ")))
-			ft_print_result("NULL");
-		else
-			ft_print_result(strtrim);
-		if (strtrim == s1)
-			ft_print_result("\nA new string was not returned");
-	}
-	return (0);
-}

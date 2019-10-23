cf3dd6804983e6cba3fe58717ed55da011f4a596
Siméon F <Shimon42@users.noreply.github.com>
2019-10-23 14:52:12 +0200

    ok

--- ft_lstlast_bonus.c
+++ ft_lstlast_bonus.c
@@ -6,7 +6,7 @@
 /*   By: siferrar <siferrar@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/19 02:52:33 by siferrar     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/21 17:41:08 by siferrar    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/23 14:51:45 by siferrar    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -19,79 +19,7 @@ t_list		*ft_lstlast(t_list *lst)
 		return (lst);
 	while (lst->next != NULL)
 	{
-		ft_putstr(lst->content);
-		ft_putstr("\n");
 		lst = lst->next;
 	}
 	return (lst);
-}
-
-t_list	*ft_lstnewone(void const *content)
-{
-	t_list	*elem;
-
-	elem = (t_list *)malloc(sizeof(t_list));
-	if (!elem)
-		return (NULL);
-	if (!content)
-	{
-		elem->content = NULL;
-	}
-	else
-	{
-		if (!(elem->content = malloc(sizeof(*(elem->content)) * sizeof(content))))
-			return (NULL);
-		elem->content = memcpy(elem->content, content, sizeof(content));
-	}
-	elem->next = NULL;
-	return (elem);
-}
-
-int main(int argc, const char *argv[])
-{
-	int			i;
-	char		*content;
-	t_list		*val;
-	t_list		*elem;
-	t_list		*elem2;
-	t_list		*elem3;
-	t_list		*elem4;
-
-	char		str [] = "lorem";
-	char		str2 [] = "ipsum";
-	char		str3 [] = "dolor";
-	char		str4 [] = "sit";
-
-	elem = ft_lstnewone(str);
-	elem2 = ft_lstnewone(str2);
-	elem3 = ft_lstnewone(str3);
-	elem4 = ft_lstnewone(str4);
-	alarm(5);
-	if (argc == 1 || !elem || !elem2 || !elem3 || !elem4)
-		return (0);
-	else if (atoi(argv[1]) == 1)
-	{
-		elem->next = elem2;
-		elem2->next = elem3;
-		elem3->next = elem4;
-		val = ft_lstlast(elem);
-		i = 0;
-		content = val->content;
-		while (content[i])
-			write(1, &(content[i++]), 1);
-		write(1, "\n", 1);
-		elem->next = NULL;
-		val = ft_lstlast(elem);
-		content = val->content;
-		i = 0;
-		while (content[i])
-			write(1, &(content[i++]), 1);
-		write(1, "\n", 1);
-		elem = NULL;
-		val = ft_lstlast(elem);
-		if (val == NULL)
-			write(1, "NULL", 4);
-		write(1, "\n", 1);
-	}
-	return (0);
 }
